a4e743b44e1161d381e909110189a84a42958527
Nosferatu <davonb113@gmail.com>
2022-12-08 11:50:07 -0600

    routine and form jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import { NewNote } from './NewNote'
 
 function App() {
 
   return (
-   <>
-   </>
+    <Container className='my-4'>
+    <Routes>
+      <Route path='*'  element={<Navigate to='/' />} />
+      <Route path='/' element={<div>Home</div>} />
+      <Route path='/new' element={<NewNote />} />
+      <Route  path='/:id'>
+        <Route index element={<h1>Show</h1>} />
+        <Route path='edit' element={<h1>Edit</h1>} />
+      </Route>
+    </Routes>
+   </Container>
   )
 }
 
