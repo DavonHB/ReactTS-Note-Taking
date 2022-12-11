1a1e01f5de8464aa459a82bf306c63348c2868a2
Nosferatu <davonb113@gmail.com>
2022-12-11 14:15:06 -0600

    Note creation logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import { NewNote } from './NewNote'
 import { useLocalStorage } from './useLocalStorage'
+import { v4 as uuidV4 } from 'uuid'
 
 // Note data with an id attached
 export type Note = {
@@ -25,7 +26,7 @@ export type Tag = {
 // Storing id of a specific tag, so when the value of a tag changes, only the tag is updated
 export type RawNote = {
   id: string
-}
+} & RawNoteData
 
 export type RawNoteData = {
   title: string
@@ -38,12 +39,28 @@ function App() {
   const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", [])
   const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", [])
 
+  // Converting raw note into a note
+  const notesWithTags = useMemo(() => {
+    // Map through each note, and for each note we are going to return every note
+    // Return tags for each note, and filter each tag that has the associated Id in the note thats being stored
+    return notes.map(note => {
+      return { ...note, tags: tags.filter(tag => note.tagIds.includes(tag.id))}
+    })
+  }, [notes, tags])
+
+  // Handles note creation
+  function onCreateNote({ tags, ...data }: NoteData) {
+    setNotes(prevNotes => { 
+      return [...prevNotes, { ...data, id: uuidV4(), tagIds: tags.map(tag => tag.id) }]
+    })
+  }
+
   return (
     <Container className='my-4'>
     <Routes>
       <Route path='*'  element={<Navigate to='/' />} />
       <Route path='/' element={<div>Home</div>} />
-      <Route path='/new' element={<NewNote />} />
+      <Route path='/new' element={<NewNote onSubmit={onCreateNote} />} />
       <Route  path='/:id'>
         <Route index element={<h1>Show</h1>} />
         <Route path='edit' element={<h1>Edit</h1>} />
